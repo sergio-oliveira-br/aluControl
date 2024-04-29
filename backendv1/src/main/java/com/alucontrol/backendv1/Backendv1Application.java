df220e95090fd5b838ce0f3499a0d5ae6d0bcb30
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-29 01:24:44 +0100

    create backendv1 and deleted backend
    I could not run the Spring Boot App

---
+++ backendv1/src/main/java/com/alucontrol/backendv1/Backendv1Application.java
@@ -0,0 +1,13 @@
+package com.alucontrol.backendv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backendv1Application.class, args);
+    }
+
+}
