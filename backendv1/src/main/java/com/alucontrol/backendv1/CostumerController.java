76131a1426ad996126002fd8d317c7307e367bde
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-29 12:57:42 +0100

    controller included
    The user input into HTML is possible to see on console by using Controller method Java (Spring Boot)

---
+++ backendv1/src/main/java/com/alucontrol/backendv1/CostumerController.java
@@ -0,0 +1,38 @@
+package com.alucontrol.backendv1;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class CostumerController
+{
+
+    @PostMapping("/saveClient")
+    public String saveClient(@RequestParam("firstName") String firstName
+                             //@RequestParam("lastName") String lastName,
+                             //@RequestParam("phoneNumber") String phoneNumber,
+                             //@RequestParam("city") String city
+    )
+    {
+
+        System.out.println("Received client data: firstName= " + firstName);
+
+        // Do something with the form data here...
+
+        return "Client data received";
+    }
+
+
+
+    /*
+    @PostMapping("/api/client")
+    public String saveClient(@RequestBody Costumer costumer)
+    {
+        System.out.println("Received client data: " + costumer);
+               return "Client data received";
+    }
+
+     */
+}
---
+++ backendv1/src/main/java/com/alucontrol/backendv1/HomeController.java
@@ -0,0 +1,14 @@
+package com.alucontrol.backendv1;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController
+{
+    @GetMapping("/")
+    public String home()
+    {
+        return "index";
+    }
+}
