6c75920ba5fccfaaab982ee88fe045cee8c51517
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-30 11:26:00 +0100

    new form implemented (products)
    now the user enter a item and by clicking on submit all info goes to mySQL

---
+++ backendv1/src/main/java/com/alucontrol/backendv1/Product.java
@@ -0,0 +1,51 @@
+/**
+ * This is for the mySQL Data Base - Table Customer
+ * Development starting on 30, April 2024
+ * Author: Sergio Oliveira
+ * Version 1.0
+ */
+
+package com.alucontrol.backendv1;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String itemDescription;
+
+    @Column(nullable = false)
+    private int itemQuantity;
+
+    //Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+}
--- backendv1/src/main/java/com/alucontrol/backendv1/ProductController.java
+++ backendv1/src/main/java/com/alucontrol/backendv1/ProductController.java
@@ -9,6 +9,35 @@
 
 package com.alucontrol.backendv1;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController
 {
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    //Need to include thr Repository
+
+    @PostMapping("/saveProduct")
+    public ResponseEntity<Product> saveProduct(@RequestParam("itemDescription") String itemDescription,
+                                               @RequestParam("itemQuantity") int itemQuantity)
+    {
+        Product product = new Product();
+        product.setItemDescription(itemDescription);
+        product.setItemQuantity(itemQuantity);
+
+        Product savedProduct = productRepository.save(product);
+
+        //Double Check
+        System.out.println("Received:" + "\n" + itemDescription);
+
+        return ResponseEntity.ok(savedProduct);
+    }
+
 }
