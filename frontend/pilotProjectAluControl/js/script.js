ca46860247a175d928ce4d28a639c18101c0bd4f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-05-02 20:04:27 +0100

    aligning the Table

--- frontend/pilotProjectAluControl/js/script.js
+++ frontend/pilotProjectAluControl/js/script.js
@@ -30,9 +30,10 @@ function loadCustomers()
             data.forEach(function(customer)
             {
                 $('#customerList').append('<tr>' +
-                                            '<td>' + customer.firstName + '<td>'+
-                                            '<td>' + customer.lastName + '<td>' +
-                                            '<td>' + customer.city + '<td>' +
+                                                '<td>' + customer.id + '</td>' +
+                                                '<td>' + customer.firstName + '<td>'+
+                                                '<td>' + customer.lastName + '<td>' +
+                                                '<td>' + customer.city + '<td>' +
                                           '</tr>');
             });
         },
