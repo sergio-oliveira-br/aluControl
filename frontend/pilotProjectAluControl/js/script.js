07d0ff0f0dd6fd899f18092740792c2a25dcd0c7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-05-20 09:55:46 +0100

    create a JS split dedicate for Rent Page
    All pages will have JS exclusive and one page will contain share features

--- frontend/pilotProjectAluControl/js/script.js
+++ frontend/pilotProjectAluControl/js/script.js
@@ -21,9 +21,6 @@ $(document).ready(function()
     //Page: Customer
     loadCustomers(); //this is the table of all my customers
 
-    //Page: Rent
-    loadRent();
-
     //Page: Product
     loadProduct();
 
@@ -87,73 +84,6 @@ function loadNumberCustomers()
 
 
 
-
-/**
-     Page: Rent
-     Item: Table
-     Method: Create a table with
-     all customers via AJAX
- */
-function loadRent()
-{
-    $.ajax({url: "/rent", type: "GET", success: function(data)
-        {
-            //first clean
-            $('#rentList').empty();
-
-            //Iteration
-            data.forEach(function(rent)
-            {
-                $('#rentList').append('<tr>' +
-                    '<td>' + rent.id + '</td>' +
-                    '<td>' + rent.rentFirstName + '</td>' +
-                    '<td>' + rent.rentLastName + '</td>' +
-                    '<td>' + rent.rentAddress + '</td>' +
-                    '<td>' + rent.rentItem + '</td>' +
-                    '<td>' + rent.rentPrice + '</td>' +
-                    '<td>' + rent.rentStarts + '</td>' +
-                    '<td>' + rent.rentEnds + '</td>' +
-                    '<td>' + rent.rentTotalPrice + '</td>'
-                );
-            });
-        },
-        error: function(xhr, status, error)
-        {
-            console.error(error);
-        }
-    });
-}
-
-/**
-     Page: Rent
-     Item: Form -> Days = (End - Start)
-     Method: Simple math to calculate the days
-*/
-//Making the Math
-function mathDays(start, end)
-{
-    let start_Date = new Date(start);
-    let end_Date = new Date(end);
-    let diffTime = Math.abs(end_Date - start_Date);
-    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return diffDays;
-}
-//Using the Method
-function loadRentDays()
-{
-    //Variables
-    let newStart = document.getElementById('rentStarts').value;
-    let newEnd = document.getElementById('rentEnds').value;
-
-    //Call the method
-    let rentTotalDays = mathDays(newEnd, newStart);
-
-    //Write in the field
-    document.getElementById('rentTotalDays').value = rentTotalDays;
-}
-//Update everytime that one of these two field is changed
-document.getElementById('rentStarts').addEventListener('change',loadRentDays);
-document.getElementById('rentEnds').addEventListener('change',loadRentDays);
 
 
 /**
