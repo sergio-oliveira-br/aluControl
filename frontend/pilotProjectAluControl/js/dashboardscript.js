6a22449a44f95d669f3492758bf5ac65144aafac
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-05-29 10:33:51 +0100

    working on dashboard - not finished yet

---
+++ frontend/pilotProjectAluControl/js/dashboardscript.js
@@ -0,0 +1,35 @@
+/**
+ Page: Dashboard
+ Item: Chart
+ Method: Create the Chart
+*/
+window.onload = function () {
+    // Graphs
+    const ctx = document.getElementById('myChart')
+    const myChart = new Chart(ctx,{
+        type: 'line',
+        data: {
+            labels:['Monday', 'Sunday', 'Tuesday', 'Wednesday', 'Thursday'],
+            dataset: [{
+                data: [111, 222, 333, 444],
+                backgroundColor: 'red',
+                lineTension: 0,
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }],
+        },
+        options:{
+            plugins:{
+                legend:{
+                    display: true,
+                },
+                tooltip:{
+                    boxPadding: '5px',
+                },
+            }
+        }
+    });
+}
+
+
