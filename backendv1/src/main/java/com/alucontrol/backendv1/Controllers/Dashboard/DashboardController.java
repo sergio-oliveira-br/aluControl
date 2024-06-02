899763fbdbeaa03725d33ac3d724c4d227981478
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-02 09:30:08 +0100

    modifying the comments in the header
    (dashboard)

--- backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Dashboard/DashboardController.java
+++ backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Dashboard/DashboardController.java
@@ -1,9 +1,12 @@
 /**
- * This is for the mySQL Data Base - Table Product/Items
- *      >> Class Controller <<
- * Development starting on 30, April 2024
- * Author: Sergio Oliveira
- * Version 1.0
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie
+ * Project Commencing May 2024
+ * Version: 1.0
  */
 package com.alucontrol.backendv1.Controllers.Dashboard;
 
