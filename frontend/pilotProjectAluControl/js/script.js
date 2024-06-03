78a9a2d6e4784c3b44e9c57a8140a093f21ec548
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-03 10:32:03 +0100

    handling exception on the rent form
    now the user will get feedback if there are something wrong for each field entered

--- frontend/pilotProjectAluControl/js/script.js
+++ frontend/pilotProjectAluControl/js/script.js
@@ -1,1 +1,29 @@
-// script.js
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie
+ * Project Commencing May 2024
+ * Version: 1.0
+ */
+//Reference: https://getbootstrap.com/docs/5.0/forms/validation/
+//Checking form submissions if there are invalid fields: Used by "Rent Form"/"Rent Form Edit"
+(function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                    console.log('Checking the form...')
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
