42e560c8cd99a0a38dab1b89005e14162a4e3ebf
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-04 09:56:52 +0100

    Update DashboardController.java
    handling exceptions on dashcontroller

--- backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Dashboard/DashboardController.java
+++ backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Dashboard/DashboardController.java
@@ -10,6 +10,7 @@
  */
 package com.alucontrol.backendv1.Controllers.Dashboard;
 
+import com.alucontrol.backendv1.Controllers.Exception.ResourceNotFoundException;
 import com.alucontrol.backendv1.Model.Rent;
 import com.alucontrol.backendv1.Projection.ItemsTPriceProjection;
 import com.alucontrol.backendv1.Projection.TotalRentProjection;
@@ -38,33 +39,53 @@ public class DashboardController
 
     /**
      * Endpoint to get back the items Rented from DB
+     * I'm not using this anymore
      */
-    @GetMapping("/allRentedItems")
-    public ResponseEntity<List<Rent>> getAllRentedItems()
-    {
-        List<Rent> myList = dashboardRepository.findAll();
-        return ResponseEntity.ok(myList);
-    }
+//    @GetMapping("/allRentedItems")
+//    public ResponseEntity<List<Rent>> getAllRentedItems()
+//    {
+//        List<Rent> myList = dashboardRepository.findAll();
+//        return ResponseEntity.ok(myList);
+//    }
 
-    /** Endpoint to get items and SUM of total prices */
+    /** Endpoint to get items and SUM of total prices
+     * Pointing to dashboardScript.js */
     @GetMapping("/findItemsTotalPrice")
+    //A Projection interface aims to determine which fields of an entity or dataset are to be selected or projected during a query
     public List<ItemsTPriceProjection> getItemsTPriceProjection()
     {
+        //handling exceptions
+        if(dashboardRepository.findItemsTotalPrice() == null)
+        {
+            throw new ResourceNotFoundException("From DashboardController: It was not possible to locate items to calculate the total price");
+        }
         return dashboardRepository.findItemsTotalPrice();
     }
 
-    /** Endpoint to get items and total price individually */
+    /** Endpoint to get items and total price individually
+     * Pointing to dashboardScript.js */
     @GetMapping("/findRentItems")
+    //A Projection interface aims to determine which fields of an entity or dataset are to be selected or projected during a query
     public List<ItemsTPriceProjection> getRentItems()
     {
+        //exception handling
+        if(dashboardRepository.findRentItems() == null)
+        {
+            throw new ResourceNotFoundException("From DashboardController: Rent items not found");
+        }
         return dashboardRepository.findRentItems();
     }
 
-    /** Endpoint to get all rent separated by status */
+    /** Endpoint to get all rent separated by status
+     * Pointing to dashboardScript.js */
     @GetMapping("/findRentPaymentStatus")
+    //A Projection interface aims to determine which fields of an entity or dataset are to be selected or projected during a query
     public List<TotalRentProjection> getRentPayment()
     {
+        if(dashboardRepository.findRentPaymentStatus() == null)
+        {
+            throw new ResourceNotFoundException("From DashboardController: Rent payment status not found");
+        }
         return dashboardRepository.findRentPaymentStatus();
     }
-
 }
