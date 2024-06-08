febb4379489dc37307b7c92ec9c9cb421a76343b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-08 09:50:28 +0100

    working on good practices
    -> comments added
    -> log added

--- backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Rent/RentReadController.java
+++ backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Rent/RentReadController.java
@@ -12,14 +12,15 @@ package com.alucontrol.backendv1.Controllers.Rent;
 
 import com.alucontrol.backendv1.Model.Rent;
 import com.alucontrol.backendv1.Repository.RentRepository;
+import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
 /** This controller is dedicated to endpoints that read data */
+@RestController
 public class RentReadController
 {
     //Repository for access to product data
@@ -35,7 +36,11 @@ public class RentReadController
     @GetMapping("/rent")
     public ResponseEntity<List<Rent>> getAllRent()
     {
-        List<Rent> rent = rentRepository.findAll();
+        LoggerUtil.info("Fetching all rent records"); //Create log
+        List<Rent> rent = rentRepository.findAll(); //using the method findALL from CRUD
+        LoggerUtil.info("Fetched " + rent.size() + " rent records"); //Create a log
+
+        //Returning the list of all rent records in a ResponseEntity with status OK
         return ResponseEntity.ok(rent);
     }
 }
