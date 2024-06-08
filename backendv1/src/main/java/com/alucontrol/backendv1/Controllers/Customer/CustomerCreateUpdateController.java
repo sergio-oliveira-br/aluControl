8368f5dc0bbc6c68739ea01bd34377513ec2269b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-08 10:49:49 +0100

    working on good practices
    -> comments added
    -> log added
    -> handled exceptions

--- backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Customer/CustomerCreateUpdateController.java
+++ backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Customer/CustomerCreateUpdateController.java
@@ -13,10 +13,13 @@ package com.alucontrol.backendv1.Controllers.Customer;
 
 import com.alucontrol.backendv1.Repository.CustomerRepository;
 import com.alucontrol.backendv1.Model.Customer;
+import com.alucontrol.backendv1.Util.LoggerUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-/**This controller is dedicated to endpoints that create and update records*/
+/**This controller is dedicated to endpoints that create and update records
+ * It is the responsibility of this layer to receive requests, call methods from the service layer, and return HTTP responses */
 @RestController
 public class CustomerCreateUpdateController
 {
@@ -36,24 +39,25 @@ public class CustomerCreateUpdateController
                                                  @RequestParam("additionalInfo") String additionalInfo,
                                                  @RequestParam("city") String city)
     {
+        try {
+            //Create a new customer object and set its attributes
+            Customer customer = new Customer();
+            customer.setFirstName(firstName);
+            customer.setLastName(lastName);
+            customer.setPhoneNumber(phoneNumber);
+            customer.setAdditionalInfo(additionalInfo);
+            customer.setCity(city);
 
-         Customer customer = new Customer();
-         customer.setFirstName(firstName);
-         customer.setLastName(lastName);
-         customer.setPhoneNumber(phoneNumber);
-         customer.setAdditionalInfo(additionalInfo);
-         customer.setCity(city);
+            //Save the customer in the database
+            Customer savedCustomer = customerRepository.save(customer);
+            LoggerUtil.info("Customer saved successfully" + customer.getFirstName()); //create a log
 
-         Customer savedCustomer = customerRepository.save(customer);
+            return ResponseEntity.ok(savedCustomer); //return the saved customer data
 
-
-        System.out.println("\nReceived client data: " +
-                "\n" + firstName +
-                "\n" + lastName +
-                "\n" + phoneNumber +
-                "\n" + additionalInfo +
-                "\n" + city);
-
-        return ResponseEntity.ok(savedCustomer);
+        }
+        catch (Exception e) {
+            LoggerUtil.error("An error occurred while saving customer data: " + e.getMessage(), e); //create log
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null); //Return an internal error
+        }
     }
 }
--- backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Product/ProductStatisticsController.java
+++ backendv1/src/main/java/com/alucontrol/backendv1/Controllers/Product/ProductStatisticsController.java
@@ -29,7 +29,4 @@ public class ProductStatisticsController
     {
         this.productRepository = productRepository;
     }
-
-    
-
 }
